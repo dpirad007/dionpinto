36b2f80e24076cdb5d95d5a97b179c3e456fc7fe
dpirad007 <dionpinto000@gmail.com>
2022-07-19 12:24:06 +0530

    maxSubArray updated

--- src/routes/blind75/maxSubarray.svelte
+++ src/routes/blind75/maxSubarray.svelte
@@ -27,6 +27,7 @@
 		</div>
 		<div id="index" class="nes-container is-rounded is-dark">
 			<div><a href="#description">Description</a></div>
+			<div><a href="#explanation">Explanation</a></div>
 			<div><a href="#code">Code</a></div>
 		</div>
 
@@ -40,6 +41,60 @@
 				<a id="link" href="https://leetcode.com/problems/maximum-subarray/" target="_blank"
 					>(Problem)</a
 				>
+			</p>
+		</section>
+
+		<section id="explanation">
+			<Title type={5} title="Explanation" />
+
+			<p>
+				We can find the max subarray by checking all possible subarrays of the array, in python this
+				can be done quite easily via the slicing operation.
+			</p>
+			<pre id="block"><code>
+				{`
+	class Solution:
+		def maxSubArray(self, nums: List[int]) -> int:
+			maxval=nums[0]
+			for i in range(len(nums)-2):
+				for j in range(i+1,len(nums)-1):
+					val = sum(nums[i:j])
+					maxval = max(val,maxval)
+			return maxval
+				`}			
+			</code></pre>
+
+			<p>
+				However the time complexity of such an approach is very poor O(n<sup>3</sup>). This is
+				beacuse even the sum() function take O(n) to compute.
+			</p>
+			<p>A better approach is to always check the current sum of the array.</p>
+			<pre id="block"><code>
+				{`
+	[-2,1,-3,4,-1,2,1,-5,4]
+
+	initially -2 -> -1 -> -4 ...
+				`}			
+			</code></pre>
+			<p>
+				We can notice that -2 did not help us at all, all it did was lower the value, hence whenever
+				we encounter a negative current sum we should set to 0.
+			</p>
+			<p>
+				We can then just iterate over the array and add the value present in ith position, followed
+				by checking the max(current sum, maximun value found so far).
+			</p>
+			<pre id="block"><code>
+				{`
+	[-2,-3,-1,-5]
+				`}			
+			</code></pre>
+			<p>
+				Suppose we get the above scenario where all the numbers are negative, then we just need to
+				get the highest value. This can be achieved by initially setting the maxval as the first
+				number of the array, after which the current sum will only decrease -> it will set to 0,
+				then it will add the negative value in ith position -> compare itself to maxval and the
+				higher number will be chosen.
 			</p>
 		</section>
 
